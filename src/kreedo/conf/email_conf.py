d11e006c99d7c5eeac60ff1a6cc626d970179c11
udayaditya singh <udayaditya.singh@gmail.com>
2021-04-21 17:29:43 +0530

    "changes"

--- src/kreedo/conf/email_conf.py
+++ src/kreedo/conf/email_conf.py
@@ -9,11 +9,11 @@ env = environ.Env(
 environ.Env.read_env('.env')
 
 
-EMAIL_PORT = 587
-# EMAIL_PORT = 465
+# EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_USE_SSL = True
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+# EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
