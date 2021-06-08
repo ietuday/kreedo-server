ccdf7c9572be175ea68a122babc44a76af4aad1b
Mayuri Waghmare <mayuriw77@gmail.com>
2021-06-08 16:26:11 +0530

    create api of holiday list and week off list

--- src/holiday/api/urls.py
+++ src/holiday/api/urls.py
@@ -7,20 +7,25 @@ urlpatterns = [
      path('holiday_type_list',
          HolidayTypeListCreate.as_view(), name='HolidayTypeListCreate'),
      
-     path('school-holiday_list_by_school/<int:pk>', SchoolHolidayListBySchool.as_view(),
+     path('school_holiday_list_by_school/<int:pk>', SchoolHolidayListBySchool.as_view(),
           name='SchoolHolidayListBySchool'),
 
-     path('school-holiday_list_create',
+     path('school_holiday_list_create',
           SchoolHolidayListCreate.as_view(), name='SchoolHolidayListCreate'),
-     path('school-holiday_retrive_update_delete/<int:pk>', SchoolHolidayRetriveUpdateDestroy.as_view(),
+     path('school_holiday_retrive_update_delete/<int:pk>', SchoolHolidayRetriveUpdateDestroy.as_view(),
           name='SchoolHolidayRetriveUpdateDestroy'),
-     path('school-weak-off_list_create',
+     path('school_weak-off_list_create',
           SchoolWeakOffListCreate.as_view(), name='SchoolWeakOffListCreate'),
-     path('school-weak-off_retrive_update_delete/<int:pk>', SchoolWeakOffRetriveUpdateDestroy.as_view(),
+     path('school_weak-off_retrive_update_delete/<int:pk>', SchoolWeakOffRetriveUpdateDestroy.as_view(),
           name='SchoolWeakOffRetriveUpdateDestroy'),
      path('calendar', Calendar.as_view(), name='Calendar'),
-     path('holiday_list_by_date_and_type', HolidayListByType.as_view(), name='HolidayListByType')
+     path('holiday_list_by_date_and_type', HolidayListByType.as_view(), name='HolidayListByType'),
 
+     path('week_off_by_academic_session/<int:pk>', SchoolWeakOffByAcademicSession.as_view(),
+          name='SchoolWeakOffByAcademicSession'),
+
+     path('holiday_list_by_academic_session/<int:pk>', HolidayListByAcademicSession.as_view(),
+          name='HolidayListByAcademicSession'),
 
 
 
