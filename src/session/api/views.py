9956de8317f8a413de677bee529a2b36a6b2496d
Mayuri Waghmare <mayuriw77@gmail.com>
2021-06-14 15:37:03 +0530

    changes in api

--- src/session/api/views.py
+++ src/session/api/views.py
@@ -86,6 +86,9 @@ class AcademicSessionListCreate(GeneralClass, Mixins, ListCreateAPIView):
             return Response(ex, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+
+
+
 """ AcademicSession Retrive Update Delete """
 
 
