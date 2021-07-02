b39c2e155035d6ebcc09532a8ebec5cb6e0adece
Mayuri Waghmare <mayuriw77@gmail.com>
2021-07-02 18:36:19 +0530

    eddofun api

--- src/users/api/edoofun_views.py
+++ src/users/api/edoofun_views.py
@@ -1,59 +1,41 @@
 """
     DJANGO LIBRARY IMPORT
 """
-import math
-import pdb
-import csv
-from pandas import DataFrame
-import json
-from .serializer import*
+
 from ..models import*
 from .filters import*
+from .edoofun_serializer import*
 from kreedo.general_views import Mixins, GeneralClass
-from kreedo.conf.logger import CustomFormatter
+from users.api.custum_storage import FileStorage
+
 import traceback
 import datetime
-import logging
-import pandas as pd
-
 import random
-from kreedo.settings import AWS_SNS_CLIENT, EMAIL_HOST_USER
-from passlib.hash import pbkdf2_sha256
 
+""" Rest File """
 from rest_framework .generics import ListCreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import (AllowAny, IsAdminUser, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
+
+""" django file """                               
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from rest_framework.decorators import permission_classes
 from django.core.exceptions import ValidationError
 from django.shortcuts import render
-from users.api.custum_storage import FileStorage
-from .edoofun_serializer import*
 
 
-""" Create Log for Edoo-Fun  Serializer"""
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler('edoofun_scheduler.log')
-handler.setLevel(logging.DEBUG)
-handler.setFormatter(CustomFormatter())
-
-logger.addHandler(handler)
-# A string with a variable at the "info" level
-logger.info("Serailizer CAlled ")
 
 
 """  Register Parent """
 
 
-class RegisterParent(GeneralClass, Mixins, ListCreateAPIView):
+class RegisterParent(ListCreateAPIView):
     def post(self, request):
         try:
-            # print(requjest)
+            
             user_data = {
                 "username": "test",
                 "first_name": request.data.get('first_name', None),
@@ -65,6 +47,7 @@ class RegisterParent(GeneralClass, Mixins, ListCreateAPIView):
                 "photo": request.data.get('photo', None),
                 "phone": request.data.get('phone', None),
                 "relationship_with_child": request.data.get('relationship_with_child', None),
+                "role":[Role.objects.filter(name="Primary")[0].id]
             }
 
             """  Pass dictionary through Context """
@@ -75,21 +58,27 @@ class RegisterParent(GeneralClass, Mixins, ListCreateAPIView):
             user_detail_serialzer = RegisterParentSerializer(
                 data=dict(user_data), context=context)
   
-
             if user_detail_serialzer.is_valid():
                 user_detail_serialzer.save()
-                return Response(user_detail_serialzer.data, status=status.HTTP_200_OK)
+            
+                context = {"isSuccess": True, "message": "Parent created sucessfully",
+                "error":"", "data": user_detail_serialzer.data, "status":status.HTTP_200_OK}
+                return Response(context, status=status.HTTP_200_OK)
+
             else:
-                print("user_detail_serialzer.errors------>", user_detail_serialzer.errors)
-                logger.debug(user_detail_serialzer.errors)
-                return Response(user_detail_serialzer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+           
+                context = {"isSuccess": False, "message": "Issue in Parent Creation",
+                "error": user_detail_serialzer.errors, "data": "","status":status.HTTP_500_INTERNAL_SERVER_ERROR}
+                return Response(context, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         except Exception as ex:
             print("Error------>", ex)
             print("TRACEBACK---------->", traceback.print_exc())
-            logger.debug(ex)
+           
+            context = {"isSuccess": False, "message": "Issue in Parent Creation","status":status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "error": ex, "data": ""}
+            return Response(context, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-            return Response(ex, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
