6995a36def32509b2abc174ed175a87ca9aa0cfe
Mayuri Waghmare <mayuriw77@gmail.com>
2021-07-06 12:50:32 +0530

    changes

--- src/child/api/edoofun_views.py
+++ src/child/api/edoofun_views.py
@@ -11,7 +11,7 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from kreedo.general_views import*
 from child.models import*
-from .serializer import*
+from .edoofun_serializer import*
 from .filters import*
 from session.models import*
 from schools.models import*
@@ -45,10 +45,49 @@ logger.info("UTILS CAlled ")
 class RegisterChild(ListCreateAPIView):
     def post(self, request):
         try:
-            
-            
 
+            child_detail = {
+                "photo": request.data.get('photo', None),
+                "first_name": request.data.get('first_name', None),
+                "last_name": request.data.get('last_name', None),
+                "date_of_birth": request.data.get('date_of_birth', None),
+                "gender": request.data.get('gender', None),
+                "date_of_joining": request.data.get('date_of_joining', None),
+                "place_of_birth": request.data.get('place_of_birth', None),
+                "blood_group": request.data.get('blood_group', None)
+            }
 
+            parent_detail = {
+                "parents": request.data.get('parents', None)
+            }
+            academic_session_detail = {
+                "academic_session": request.data.get('academic_session', None),
+                "section": request.data.get('section', None),
+                "grade": request.data.get('grade', None),
+                "class_teacher": request.data.get('class_teacher', None),
+                "curriculum_start_date": request.data.get('curriculum_start_date', None),
+                "subjects": request.data.get('subjects', None)
+
+            }
+
+            """  Pass dictionary through Context """
+            context = super().get_serializer_context()
+            context.update(
+                {"child_detail": child_detail, "parent_detail": parent_detail,
+                 "academic_session_detail": academic_session_detail})
+            try:
+
+                child_detail_serializer = ChildRegisterSerializer(
+                    data=dict(child_detail), context=context)
+                if child_detail_serializer.is_valid():
+                    child_detail_serializer.save()
+                    return Response(child_detail_serializer.data)
+                else:
+                    return Response(child_detail_serializer.errors)
+
+            except Exception as ex:
+                logger.info(ex)
+                logger.debug(ex)
 
         except Exception as ex:
             context = {"isSuccess": False, "message": "Issue in Child Creation", "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
--- src/period/api/urls.py
+++ src/period/api/urls.py
@@ -3,51 +3,51 @@ from .views import*
 
 
 urlpatterns = [
-     path('period-template_list_create',
-          PeriodTemplateListCreate.as_view(), name='PeriodTemplateListCreate'),
-     path('period-template_retrive_update_delete/<int:pk>', PeriodTemplateRetriveUpdateDestroy.as_view(),
-          name='PeriodTemplateRetriveUpdateDestroy'),
+    path('period-template_list_create',
+         PeriodTemplateListCreate.as_view(), name='PeriodTemplateListCreate'),
+    path('period-template_retrive_update_delete/<int:pk>', PeriodTemplateRetriveUpdateDestroy.as_view(),
+         name='PeriodTemplateRetriveUpdateDestroy'),
 
-     path('period-template-detail_list_create',
-          PeriodTemplateDetailListCreate.as_view(), name='PeriodTemplateDetailListCreate'),
+    path('period-template-detail_list_create',
+         PeriodTemplateDetailListCreate.as_view(), name='PeriodTemplateDetailListCreate'),
 
-     path('period-template-detail_retrive_update_delete/<int:pk>', PeriodTemplateDetailRetriveUpdateDestroy.as_view(),
-          name='PeriodTemplateDetailRetriveUpdateDestroy'),
+    path('period-template-detail_retrive_update_delete/<int:pk>', PeriodTemplateDetailRetriveUpdateDestroy.as_view(),
+         name='PeriodTemplateDetailRetriveUpdateDestroy'),
 
-     path('period_list_create',
-          PeriodListCreate.as_view(), name='PeriodListCreate'),
-
-
-     path('period_retrive_update_delete/<int:pk>', PeriodRetriveUpdateDestroy.as_view(),
-          name='PeriodRetriveUpdateDestroy'),
-
-     path('classes_by_teacher_list',
-          ClassAccordingToTeacher.as_view(), name='ClassAccordingToTeacher'),
-
-     path('activity_by_child', ActivityByChild.as_view(), name='ActivityByChild'),
-     path('activity_list_by_child', ActivityListByChild.as_view(),
-          name='ActivityListByChild'),
+    path('period_list_create',
+         PeriodListCreate.as_view(), name='PeriodListCreate'),
 
 
-     path('activity-detail_retrive_by_child/<int:pk>', ActivityDetail.as_view(),
-          name='ActivityDetail'),
-     path('period_template_apply_to_grade', PeriodTemplateAppyToGradesListCreate.as_view(),
-          name='PeriodTemplateAppyToGradesListCreate'),
+    path('period_retrive_update_delete/<int:pk>', PeriodRetriveUpdateDestroy.as_view(),
+         name='PeriodRetriveUpdateDestroy'),
+
+    path('classes_by_teacher_list',
+         ClassAccordingToTeacher.as_view(), name='ClassAccordingToTeacher'),
+
+    path('activity_by_child', ActivityByChild.as_view(), name='ActivityByChild'),
+    path('activity_list_by_child', ActivityListByChild.as_view(),
+         name='ActivityListByChild'),
 
 
-     path('period_template_apply_to_grade_retrive_update_delete/<int:pk>', PeriodTemplateAppyToGradesRetriveUpdateDestroy.as_view(),
-          name='PeriodTemplateAppyToGradesRetriveUpdateDestroy'),
-     path('period_create',
-          PeriodCreate.as_view(), name='PeriodCreate'),
-     path('period_month_list',
-          PeriodMonthList.as_view(), name='PeriodMonthList'),
-     path('period_list_according_to_date',
-          PerioListAccordingDate.as_view(), name='PerioListAccordingDate'),
-     path('period_template_detail_by_period_template/<int:pk>', PeriodTemplateDetailByPeriodTemplate.as_view(),
-          name='PeriodTemplateDetailByPeriodTemplate'),
-     path('get_period_count_by_academic_session',
-          PeriodCountListByAcademicSession.as_view(), name='PeriodCountListByAcademicSession'),
+    path('activity-detail_retrive_by_child/<int:pk>', ActivityDetail.as_view(),
+         name='ActivityDetail'),
+    path('period_template_apply_to_grade', PeriodTemplateAppyToGradesListCreate.as_view(),
+         name='PeriodTemplateAppyToGradesListCreate'),
 
 
-     
+    path('period_template_apply_to_grade_retrive_update_delete/<int:pk>', PeriodTemplateAppyToGradesRetriveUpdateDestroy.as_view(),
+         name='PeriodTemplateAppyToGradesRetriveUpdateDestroy'),
+    path('period_create',
+         PeriodCreate.as_view(), name='PeriodCreate'),
+    path('period_month_list',
+         PeriodMonthList.as_view(), name='PeriodMonthList'),
+    path('period_list_according_to_date',
+         PerioListAccordingDate.as_view(), name='PerioListAccordingDate'),
+    path('period_template_detail_by_period_template/<int:pk>', PeriodTemplateDetailByPeriodTemplate.as_view(),
+         name='PeriodTemplateDetailByPeriodTemplate'),
+    path('get_period_count_by_academic_session',
+         PeriodCountListByAcademicSession.as_view(), name='PeriodCountListByAcademicSession'),
+
+
+
 ]
