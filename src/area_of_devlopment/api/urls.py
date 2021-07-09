53b5c898e7be24cedd31a237e1d1c5e76d1b9e71
Mayuri Waghmare <mayuriw77@gmail.com>
2021-07-09 16:31:11 +0530

    api created

--- src/area_of_devlopment/api/urls.py
+++ src/area_of_devlopment/api/urls.py
@@ -1,33 +1,30 @@
 from django.urls import path
 from .views import*
-
+from .edoofun_views import*
 
 urlpatterns = [
-    path('area-of-devlopment_list_create',
-         AreaOfDevlopmentListCreate.as_view(), name='AreaOfDevlopmentListCreate'),
+     path('area-of-devlopment_list_create',
+          AreaOfDevlopmentListCreate.as_view(), name='AreaOfDevlopmentListCreate'),
 
-    path('area-of-devlopment_retrive_update_delete/<int:pk>', AreaOfDevlopmentRetriveUpdateDestroy.as_view(),
-         name='AreaOfDevlopmentRetriveUpdateDestroy'),
+     path('area-of-devlopment_retrive_update_delete/<int:pk>', AreaOfDevlopmentRetriveUpdateDestroy.as_view(),
+          name='AreaOfDevlopmentRetriveUpdateDestroy'),
 
-    path('concept_list_create',
-         ConceptListCreate.as_view(), name='ConceptListCreate'),
+     path('concept_list_create',
+          ConceptListCreate.as_view(), name='ConceptListCreate'),
 
-    path('concept_retrive_update_delete/<int:pk>', ConceptRetriveUpdateDestroy.as_view(),
-         name='ConceptRetriveUpdateDestroy'),
+     path('concept_retrive_update_delete/<int:pk>', ConceptRetriveUpdateDestroy.as_view(),
+          name='ConceptRetriveUpdateDestroy'),
 
-    path('skill_list_create',
-         SkillListCreate.as_view(), name='SkillListCreate'),
+     path('skill_list_create',
+          SkillListCreate.as_view(), name='SkillListCreate'),
 
-    path('skill_retrive_update_delete/<int:pk>', SkillRetriveUpdateDestroy.as_view(),
-         name='SkillRetriveUpdateDestroy'),
-path('bulk-upload/add-concept-skill',
-         AddConceptSkill.as_view(), name='AddConceptSkill'),
-
-
-
-
-
-
+     path('skill_retrive_update_delete/<int:pk>', SkillRetriveUpdateDestroy.as_view(),
+          name='SkillRetriveUpdateDestroy'),
+     path('bulk-upload/add-concept-skill',
+          AddConceptSkill.as_view(), name='AddConceptSkill'),
+          
+     path('edoofun/skill_list_by_concept/<int:pk>',
+          SkillListByConcept.as_view(), name='Skill List By Concept'),
 
 
 ]
