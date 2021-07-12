7550b4930a43c337f7c88ce0e62550680b0d07ec
Mayuri Waghmare <mayuriw77@gmail.com>
2021-07-12 10:44:45 +0530

    create api of section list by school

--- src/session/api/edoofun_views.py
+++ src/session/api/edoofun_views.py
@@ -18,18 +18,28 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-
-
-
 """ get Section based on school """
+
+
 class SectionListBySchool(ListCreateAPIView):
-    def get(self,request,pk):
+    def get(self, request, pk):
         try:
             section_qs = AcademicSession.objects.filter(school=pk)
-            
+            if section_qs:
+                section_qs_serializer = SectionListBySchoolSerializer(
+                    section_qs, many=True)
+
+                context = {'isSuccess': True, 'message': "Section List",
+                           'data': section_qs_serializer.data, "statusCode": status.HTTP_200_OK}
+                return Response(context, status=status.HTTP_200_OK)
+            else:
+
+                context = {'isSuccess': True, 'message': "Section List Not Found",
+                           'data': " ", "statusCode": status.HTTP_404_NOT_FOUND}
+                return Response(context, status=status.HTTP_404_NOT_FOUND)
+
         except Exception as ex:
-        
+
             context = {'isSuccess': False, "error": ex,
-                        "statusCode": status.HTTP_500_INTERNAL_SERVER_ERROR,'data':''}
-            return Response(context,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            
+                       "statusCode": status.HTTP_500_INTERNAL_SERVER_ERROR, 'data': ''}
+            return Response(context, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
